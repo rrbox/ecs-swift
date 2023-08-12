008cd9caf1b3e8b0e4fd36b6d35912f95fdebd44
rrbox <87851278+rrbox@users.noreply.github.com>
2023-08-12 16:39:22 +0900

    add resource

--- Sources/ECS/Resource/Commands+Resource.swift
+++ Sources/ECS/Resource/Commands+Resource.swift
@@ -1,8 +1,27 @@
 //
-//  File.swift
+//  Commands+Resource.swift
 //  
 //
 //  Created by rrbox on 2023/08/12.
 //
 
-import Foundation
+class AddResource<T: ResourceProtocol>: Command {
+    let resrouce: T
+    
+    init(resrouce: T) {
+        self.resrouce = resrouce
+    }
+    
+    override func runCommand(in world: World) {
+        world.addResource(self.resrouce)
+    }
+}
+
+public extension Commands {
+    /// world に対して resource を追加します.
+    ///
+    /// resource はフレームの終了直前に追加されます.
+    func addResource<T: ResourceProtocol>(_ resource: T) {
+        self.push(command: AddResource(resrouce: resource))
+    }
+}
