c6b52ede26d1664ea490d06bc523a94268ed9374
rrbox <87851278+rrbox@users.noreply.github.com>
2023-08-16 07:54:17 +0900

    add struct Entities

--- Sources/ECS/World/World.swift
+++ Sources/ECS/World/World.swift
@@ -5,12 +5,28 @@
 //  Created by rrbox on 2023/08/06.
 //
 
+public struct Entities {
+    var sequence: [Entity: EntityRecord]
+    
+    public mutating func insert(entity: Entity, entityRecord: EntityRecord) {
+        self.sequence[entity] = entityRecord
+    }
+    
+    public mutating func remove(entity: Entity) {
+        self.sequence.removeValue(forKey: entity)
+    }
+    
+    public mutating func entityRecord(forEntity entity: Entity) -> EntityRecord? {
+        self.sequence[entity]
+    }
+}
+
 final public class World {
-    var entities: [Entity: Archetype]
+    var entities: Entities
     public let worldBuffer: WorldBuffer
     
-    init(entities: [Entity: Archetype], worldBuffer: WorldBuffer) {
-        self.entities = entities
+    init(entities: [Entity: EntityRecord], worldBuffer: WorldBuffer) {
+        self.entities = Entities(sequence: entities)
         self.worldBuffer = worldBuffer
     }
     
