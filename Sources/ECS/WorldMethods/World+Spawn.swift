e41179de13955e25efff8427c1c2369e9c8a72c4
rrbox <87851278+rrbox@users.noreply.github.com>
2023-09-14 06:39:15 +0900

    add: DidSpawnEvent, WillDespawnEvent

--- Sources/ECS/WorldMethods/World+Spawn.swift
+++ Sources/ECS/WorldMethods/World+Spawn.swift
@@ -5,25 +5,43 @@
 //  Created by rrbox on 2023/08/10.
 //
 
+public struct DidSpawnEvent: CommandsEventProtocol {
+    public let spawnedEntity: Entity
+}
+
+public struct WillDespawnEvent: CommandsEventProtocol {
+    public let despawnedEntity: Entity
+}
+
 extension World {
     /// Entity を登録します.
     ///
     /// ``Commands/spawn()`` が実行された後, フレームが終了するタイミングでこの関数が実行されます.
     /// entity へのコンポーネントの登録などは, push の後に行われます.
     func push(entity: Entity, entityRecord: EntityRecord) {
-        self.entities.insert(entity: entity, entityRecord: entityRecord)
+        self.insert(entity: entity, entityRecord: entityRecord)
         self.worldBuffer
             .chunkBuffer
             .push(entity: entity, entityRecord: entityRecord)
+        
+        self.worldBuffer
+            .eventBuffer
+            .commandsEventWriter(eventOfType: DidSpawnEvent.self)!
+            .send(value: DidSpawnEvent(spawnedEntity: entity))
     }
     
     /// Entity を削除します.
     ///
     /// ``Commands/despawn()`` が実行された後, フレームが終了するタイミングでこの関数が実行されます.
     func despawn(entity: Entity) {
-        self.entities.remove(entity: entity)
+        self.remove(entity: entity)
         self.worldBuffer
             .chunkBuffer
             .despawn(entity: entity)
+        
+        self.worldBuffer
+            .eventBuffer
+            .commandsEventWriter(eventOfType: WillDespawnEvent.self)!
+            .send(value: WillDespawnEvent(despawnedEntity: entity))
     }
 }
