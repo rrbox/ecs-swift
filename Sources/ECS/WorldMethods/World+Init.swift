92cf0c461f17085f332cd2bc5d84e10f353e127a
rrbox <87851278+rrbox@users.noreply.github.com>
2023-09-14 06:48:28 +0900

    change: entity count resource の追加, spawn/despawn event の設定をプロセスに追加

--- Sources/ECS/WorldMethods/World+Init.swift
+++ Sources/ECS/WorldMethods/World+Init.swift
@@ -16,11 +16,23 @@ public extension World {
         self.worldBuffer.resourceBuffer.addResource(CurrentTime(value: 0))
         self.worldBuffer.resourceBuffer.addResource(DeltaTime(value: 0))
         
+        // resrouce buffer に world の情報関係の resource を追加します.
+        self.worldBuffer.resourceBuffer.addResource(EntityCount(count: 0))
+        
         // world buffer に setup system を保持する領域を確保します.
         self.worldBuffer.systemBuffer.registerSystemRegistry(ofType: SetUpExecute.self)
         
         // world buffer に update system を保持する領域を確保します.
         self.worldBuffer.systemBuffer.registerSystemRegistry(ofType: UpdateExecute.self)
+        
+        // world buffer に event queue を作成します.
+        self.worldBuffer.eventBuffer.setUpEventQueue()
+        self.worldBuffer.eventBuffer.setUpCommandsEventQueue(eventOfType: DidSpawnEvent.self)
+        self.worldBuffer.eventBuffer.setUpCommandsEventQueue(eventOfType: WillDespawnEvent.self)
+        
+        // world buffer に spawn/despawn event の streamer を登録します.
+        self.addCommandsEventStreamer(eventType: DidSpawnEvent.self)
+        self.addCommandsEventStreamer(eventType: WillDespawnEvent.self)
         
         // world buffer に commands の初期値を設定します.
         self.worldBuffer.commandsBuffer.setCommands(Commands())
