f511dfbad8551376dd8b44b18a9492a398986760
rrbox <87851278+rrbox@users.noreply.github.com>
2023-09-14 07:02:10 +0900

    chore: get entity record method

--- Sources/ECS/WorldMethods/World+EntityCommands.swift
+++ Sources/ECS/WorldMethods/World+EntityCommands.swift
@@ -8,7 +8,7 @@
 extension World {
     /// Entity に Component を追加します.
     func addComponent<ComponentType: Component>(_ component: ComponentType, forEntity entity: Entity) {
-        let archetype = self.entities.entityRecord(forEntity: entity)!
+        let archetype = self.entityRecord(forEntity: entity)!
         if let componentRef = archetype.component(ofType: ComponentRef<ComponentType>.self) {
             componentRef.value = component
         }
@@ -18,7 +18,7 @@ extension World {
     
     /// Entity から Component を削除します.
     func removeComponent<ComponentType: Component>(ofType type: ComponentType.Type, fromEntity entity: Entity) {
-        let archetype = self.entities.entityRecord(forEntity: entity)!
+        let archetype = self.entityRecord(forEntity: entity)!
         archetype.removeComponent(ofType: ComponentRef<ComponentType>.self)
         self.worldBuffer.chunkBuffer.applyCurrentState(archetype, forEntity: entity)
     }
