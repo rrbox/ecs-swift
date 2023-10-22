b34805b1f22e5cc70b0a0db0d9e2abc222b3f213
rrbox <87851278+rrbox@users.noreply.github.com>
2023-10-22 15:17:37 +0900

    add: Filtered query

--- Sources/ECS/FilterdQuery/Filter.swift
+++ Sources/ECS/FilterdQuery/Filter.swift
@@ -1,8 +1,34 @@
 //
-//  File.swift
+//  Filter.swift
 //  
 //
 //  Created by rrbox on 2023/09/17.
 //
 
-import Foundation
+public protocol Filter {
+    static func condition(forEntityRecord entityRecord: EntityRecord) -> Bool
+}
+
+public struct With<T: Component>: Filter {
+    public static func condition(forEntityRecord entityRecord: EntityRecord) -> Bool {
+        entityRecord.component(ofType: ComponentRef<T>.self) != nil
+    }
+}
+
+public struct WithOut<T: Component>: Filter {
+    public static func condition(forEntityRecord entityRecord: EntityRecord) -> Bool {
+        entityRecord.component(ofType: ComponentRef<T>.self) == nil
+    }
+}
+
+public struct And<T: Filter, U: Filter>: Filter {
+    public static func condition(forEntityRecord entityRecord: EntityRecord) -> Bool {
+        T.condition(forEntityRecord: entityRecord) && U.condition(forEntityRecord: entityRecord)
+    }
+}
+
+public struct Or<T: Filter, U: Filter>: Filter {
+    public static func condition(forEntityRecord entityRecord: EntityRecord) -> Bool {
+        T.condition(forEntityRecord: entityRecord) || U.condition(forEntityRecord: entityRecord)
+    }
+}
--- Sources/ECS/FilterdQuery/QueryProtocol.swift
+++ Sources/ECS/FilterdQuery/QueryProtocol.swift
@@ -1,8 +1,18 @@
 //
-//  File.swift
+//  QueryProtocol.swift
 //  
 //
 //  Created by rrbox on 2023/09/17.
 //
 
-import Foundation
+public protocol QueryProtocol {
+    func spawn(entity: Entity, entityRecord: EntityRecord)
+    func despawn(entity: Entity)
+    init()
+}
+
+extension Query: QueryProtocol {}
+extension Query2: QueryProtocol {}
+extension Query3: QueryProtocol {}
+extension Query4: QueryProtocol {}
+extension Query5: QueryProtocol {}
