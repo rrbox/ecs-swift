729aed2b7f17ec829b3d8ad33ae8c459f0542960
rrbox <87851278+rrbox@users.noreply.github.com>
2023-10-24 04:43:55 +0900

    update: plug-ins

--- Sources/PlugIns/ObjectLink/ObjectLink.swift
+++ Sources/PlugIns/ObjectLink/ObjectLink.swift
@@ -6,3 +6,14 @@
 //
 
 import ECS
+
+public struct Object<T: AnyObject>: Component {
+    unowned let object: T
+    public init(object: T) {
+        self.object = object
+    }
+}
+
+extension Commands {
+    
+}
--- Sources/PlugIns/Scene/Scene.swift
+++ Sources/PlugIns/Scene/Scene.swift
@@ -5,4 +5,32 @@
 //  Created by rrbox on 2023/08/06.
 //
 
-import Foundation
+import SpriteKit
+import ECS
+
+open class Scene: SKScene {
+    var _world: World!
+    
+    open func buildWorld() -> World {
+        World()
+    }
+    
+    public var world: World {
+        self._world
+    }
+    
+    final public override func didMove(to view: SKView) {
+        self._world = self.buildWorld()
+        self._world?.setUpWorld()
+    }
+    
+    final public override func update(_ currentTime: TimeInterval) {
+        self._world?.update(currentTime: currentTime)
+    }
+    
+    public func restartWorld() {
+        self._world = self.buildWorld()
+        self._world.setUpWorld()
+    }
+    
+}
