d768f667111b7d048ab0db88961c0776a6c80f4e
rrbox <87851278+rrbox@users.noreply.github.com>
2023-10-26 03:14:19 +0900

    add: Schedule

--- Sources/ECS/Schedule/Schedule.swift
+++ Sources/ECS/Schedule/Schedule.swift
@@ -1,8 +1,20 @@
 //
-//  File.swift
-//  
+//  Schedule.swift
+//
 //
 //  Created by rrbox on 2023/10/22.
 //
 
-import Foundation
+public struct Schedule: Hashable {
+    let id: AnyHashable
+}
+
+enum DefaultSchedule {
+    case startUp
+    case update
+}
+
+public extension Schedule {
+    static let startUp: Schedule = Schedule(id: DefaultSchedule.startUp)
+    static let update: Schedule = Schedule(id: DefaultSchedule.update)
+}
