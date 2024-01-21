65b7ed39696175725a9eed09a01ba13e22c04f1d
rrbox <87851278+rrbox@users.noreply.github.com>
2024-01-21 20:58:50 +0900

    add: tests for filtered query

--- Tests/ecs-swiftTests/FilteredQueryTests.swift
+++ Tests/ecs-swiftTests/FilteredQueryTests.swift
@@ -6,30 +6,58 @@
 //
 
 import XCTest
+@testable import ECS
 
 final class FilteredQueryTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testFilteredQuery() {
+        let testQueryAnd = Filtered<Query<TestComponent>, And<With<TestComponent2>, With<TestComponent3>>>()
+        let testQueryOr = Filtered<Query<TestComponent>, Or<With<TestComponent2>, With<TestComponent3>>>()
+        let testQueryWithout = Filtered<Query<TestComponent>, And<WithOut<TestComponent2>, WithOut<TestComponent3>>>()
+        
+        let world = World()
+        
+        world.worldStorage.chunkStorage.addChunk(testQueryAnd)
+        world.worldStorage.chunkStorage.addChunk(testQueryOr)
+        world.worldStorage.chunkStorage.addChunk(testQueryWithout)
+        
+        let commands = world.worldStorage.commandsStorage.commands()!
+        
+        let entity = commands.spawn()
+            .addComponent(TestComponent(content: "c0"))
+            .id()
+        
+        world.applyCommands()
+        
+        XCTAssertEqual(testQueryAnd.query.components.count, 0)
+        XCTAssertEqual(testQueryOr.query.components.count, 0)
+        XCTAssertEqual(testQueryWithout.query.components.count, 1)
+        
+        commands.entity(entity)?.addComponent(TestComponent2(content: "c1"))
+        world.applyCommands()
+        
+        XCTAssertEqual(testQueryAnd.query.components.count, 0)
+        XCTAssertEqual(testQueryOr.query.components.count, 1)
+        XCTAssertEqual(testQueryWithout.query.components.count, 0)
+        
+        commands.entity(entity)?.addComponent(TestComponent3(content: "c2"))
+        world.applyCommands()
+        
+        XCTAssertEqual(testQueryAnd.query.components.count, 1)
+        XCTAssertEqual(testQueryOr.query.components.count, 1)
+        XCTAssertEqual(testQueryWithout.query.components.count, 0)
+        
+        commands.entity(entity)?.removeComponent(ofType: TestComponent2.self)
+        world.applyCommands()
+        
+        XCTAssertEqual(testQueryAnd.query.components.count, 0)
+        XCTAssertEqual(testQueryOr.query.components.count, 1)
+        XCTAssertEqual(testQueryWithout.query.components.count, 0)
+        
+        commands.entity(entity)?.removeComponent(ofType: TestComponent.self)
+        world.applyCommands()
+        
+        XCTAssertEqual(testQueryAnd.query.components.count, 0)
+        XCTAssertEqual(testQueryOr.query.components.count, 0)
+        XCTAssertEqual(testQueryWithout.query.components.count, 0)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
