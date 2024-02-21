da47e56ebbcaecb923edbd91c96c4ed8259bf852
rrbox <87851278+rrbox@users.noreply.github.com>
2024-02-21 19:31:36 +0900

    add: SearchEntityCommands, SpawnedEntityCommands

--- Sources/ECS/EntityCommands/EntityCommands/SearchedEntityCommands.swift
+++ Sources/ECS/EntityCommands/EntityCommands/SearchedEntityCommands.swift
@@ -1,8 +1,26 @@
 //
-//  File.swift
-//  
+//  SearchedEntityCommands.swift
+//
 //
 //  Created by rrbox on 2024/02/18.
 //
 
-import Foundation
+
+final public class SearchedEntityCommands: EntityCommands {
+    /// Entity に Component を追加します.
+    /// - Parameter component: 追加するコンポーネントを指定します.
+    /// - Returns: Entity component のビルダーです.
+    @discardableResult public override func addComponent<ComponentType: Component>(_ component: ComponentType) -> EntityCommands {
+        self.commands.componentTramsactions.append(AddComponent(entity: self.entity, component: component))
+        return self
+    }
+    
+    /// Entity から Component を削除します.
+    /// - Parameter type: 削除する Component の型を指定します.
+    /// - Returns: Entity component のビルダーです.
+    @discardableResult public override func removeComponent<ComponentType: Component>(ofType type: ComponentType.Type) -> EntityCommands {
+        self.commands.componentTramsactions.append(RemoveComponent(entity: self.entity, componentType: ComponentType.self))
+        return self
+    }
+}
+
--- Sources/ECS/EntityCommands/EntityCommands/SpawnedEntityCommands.swift
+++ Sources/ECS/EntityCommands/EntityCommands/SpawnedEntityCommands.swift
@@ -1,8 +1,25 @@
 //
-//  File.swift
+//  SpawnedEntityCommands.swift
 //  
 //
 //  Created by rrbox on 2024/02/18.
 //
 
-import Foundation
+final public class SpawnedEntityCommands: EntityCommands {
+    let record: EntityRecordRef
+    
+    init(entity: Entity, record: EntityRecordRef, commands: Commands) {
+        self.record = record
+        super.init(entity: entity, commands: commands)
+    }
+    
+    @discardableResult public override func addComponent<C: Component>(_ component: C) -> SpawnedEntityCommands {
+        self.record.addComponent(component)
+        return self
+    }
+    
+    @discardableResult public override func removeComponent<C: Component>(ofType type: C.Type) -> SpawnedEntityCommands {
+        self.record.removeComponent(ofType: C.self)
+        return self
+    }
+}
