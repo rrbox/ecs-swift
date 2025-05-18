044931f10b6c2668c26a2ab2ea3d7fc81c9fbd2a
rrbox <87851278+rrbox@users.noreply.github.com>
2025-05-18 21:32:24 +0900

    Update graphic system tests

--- Tests/GraphicPlugInTests/GraphicPlugInTests.swift
+++ Tests/GraphicPlugInTests/GraphicPlugInTests.swift
@@ -10,20 +10,21 @@ import ECS_Graphic
 import SpriteKit
 import XCTest
 
+@MainActor
 final class GraphicPlugInTests: XCTestCase {
     func testSetGraphic() {
-        let scene = SKScene()
+        let scene = SKScene() // 画面
         let world = World()
             .addResource(SceneResource(scene))
             .addPlugIn(graphicPlugIn(world:))
-            .addSystem(.startUp) { (commands: Commands) in
+            .addSystem(.startUp) { (commands: Commands, nodes: Resource<Nodes>) in
+                let node = SKNode()
                 commands.spawn()
-                    .setGraphic(SKNode())
+                    .setGraphic(nodes.resource.create(node: node))
             }
 
         world.setUpWorld()
         world.update(currentTime: -1) // first frame state
-        world.update(currentTime: 0) // update state
         XCTAssertEqual(scene.children.count, 1)
     }
 
@@ -33,28 +34,31 @@ final class GraphicPlugInTests: XCTestCase {
         let world = World()
             .addResource(SceneResource(scene))
             .addPlugIn(graphicPlugIn(world:))
-            .addSystem(.update) { (commands: Commands, currentTime: Resource<CurrentTime>) in
+            .addSystem(.update) { (commands: Commands, currentTime: Resource<CurrentTime>, nodes: Resource<Nodes>) in
                 if currentTime.resource.value == 0 {
+                    let childNode = SKNode()
                     let child = commands.spawn()
-                        .setGraphic(SKNode())
+                        .setGraphic(nodes.resource.create(node: childNode))
                         .id()
                     commands.spawn()
-                        .setGraphic(parentNode)
+                        .setGraphic(nodes.resource.create(node: parentNode))
                         .addChild(child)
                 }
             }
-            .addSystem(.update) { (children: Query<Child>, parents: Query2<Entity, Parent>, currentTime: Resource<CurrentTime>, commands: Commands) in
+            .addSystem(.update) { (
+                children: Query<Child>,
+                parents: Query3<Entity, Parent, Graphic<SKNode>>,
+                currentTime: Resource<CurrentTime>,
+                commands: Commands
+            ) in
                 switch currentTime.resource.value {
                 case -1: fatalError() // ここは通過しない.
                 case 0:
-                    break
-                case 1:
                     XCTAssertEqual(parents.components.data.count, 2)
                     XCTAssertEqual(parentNode.children.count, 1)
-                    XCTAssertEqual(children.components.data.count, 0)
-                case 2:
-                    XCTAssertEqual(parents.components.data.count, 2)
                     XCTAssertEqual(children.components.data.count, 1)
+                    break
+                case 1: break
                 default: return
                 }
             }
@@ -72,9 +76,12 @@ final class GraphicPlugInTests: XCTestCase {
         let world = World()
             .addResource(SceneResource(scene))
             .addPlugIn(graphicPlugIn(world:))
-            .addSystem(.startUp) { (commands: Commands) in
+            .addSystem(.startUp) { (commands: Commands, nodes: Resource<Nodes>) in
+                let childNode_0 = SKNode()
+                let childNode_1 = SKNode()
+
                 let child_0 = commands.spawn()
-                    .setGraphic(SKNode())
+                    .setGraphic(nodes.resource.create(node: childNode_0))
                     .id()
                 commands.spawn()
                     .addChild(child_0)
@@ -82,7 +89,7 @@ final class GraphicPlugInTests: XCTestCase {
                 let child_1 = commands.spawn()
                     .id()
                 commands.spawn()
-                    .setGraphic(SKNode())
+                    .setGraphic(nodes.resource.create(node: childNode_1))
                     .addChild(child_1)
             }
             .addSystem(.update) { (children: Query<Child>, parents: Query2<Entity, Parent>) in
@@ -108,12 +115,14 @@ final class GraphicPlugInTests: XCTestCase {
         let world = World()
             .addResource(SceneResource(scene))
             .addPlugIn(graphicPlugIn(world:))
-            .addSystem(.startUp) { (commands: Commands) in
+            .addSystem(.startUp) { (commands: Commands, nodes: Resource<Nodes>) in
+                let childNode = SKNode()
+
                 let child = commands.spawn()
-                    .setGraphic(SKNode())
+                    .setGraphic(nodes.resource.create(node: childNode))
                     .id()
                 commands.spawn()
-                    .setGraphic(parentNode)
+                    .setGraphic(nodes.resource.create(node: parentNode))
                     .addChild(child)
             }
             .addSystem(.update) { (children: Query<Child>, parents: Query2<Entity, Parent>, currentTime: Resource<CurrentTime>) in
@@ -124,9 +133,10 @@ final class GraphicPlugInTests: XCTestCase {
                 case 0:
                     flags[0] += 1
                     XCTAssertEqual(parentNode.children.count, 1)
-                    XCTAssertEqual(children.components.data.count, 0)
-                case 1:
+                    XCTAssertEqual(children.components.data.count, 1)
+                case 1: // FIXME: - 削除
                     flags[0] += 1
+                    XCTAssertEqual(parentNode.children.count, 1)
                     XCTAssertEqual(children.components.data.count, 1)
                 default: return
                 }
@@ -143,9 +153,9 @@ final class GraphicPlugInTests: XCTestCase {
                     }
                 case 3:
                     flags[1] += 1
-                    XCTAssertEqual(children.query.components.data.count, 1)
+                    XCTAssertEqual(children.query.components.data.count, 0)
                     XCTAssertEqual(parentNode.children.count, 0)
-                case 4:
+                case 4: // FIXME: - 削除
                     flags[1] += 1
                     XCTAssertEqual(children.query.components.data.count, 0)
                     XCTAssertEqual(parentNode.children.count, 0)
@@ -156,10 +166,10 @@ final class GraphicPlugInTests: XCTestCase {
         world.setUpWorld()
         world.update(currentTime: -1)
         world.update(currentTime: 0)
-        world.update(currentTime: 1)
+        world.update(currentTime: 1) // FIXME: - 削除
         world.update(currentTime: 2) // この一番最後で _remove from parent tarnsaction 追加
-        world.update(currentTime: 3) // remove from parent system 実行, 一番最後に component に変更反映
-        world.update(currentTime: 4) // ここで結果が出る
+        world.update(currentTime: 3) // remove from parent system 実行, 一番最後に component に変更反映 | ここで結果が出る
+        world.update(currentTime: 4) // FIXME: - 削除
 
         XCTAssertEqual(flags, [2, 3])
     }
@@ -173,16 +183,19 @@ final class GraphicPlugInTests: XCTestCase {
         let world = World()
             .addResource(SceneResource(scene))
             .addPlugIn(graphicPlugIn(world:))
-            .addSystem(.startUp) { (commands: Commands) in
+            .addSystem(.startUp) { (commands: Commands, nodes: Resource<Nodes>) in
+                let grandChildNode = SKNode()
+                let childNode = SKNode()
+
                 let grandchild = commands.spawn()
-                    .setGraphic(SKNode())
+                    .setGraphic(nodes.resource.create(node: grandChildNode))
                     .id()
                 let child = commands.spawn()
-                    .setGraphic(SKNode())
+                    .setGraphic(nodes.resource.create(node: childNode))
                     .addChild(grandchild)
                     .id()
                 commands.spawn()
-                    .setGraphic(parent)
+                    .setGraphic(nodes.resource.create(node: parent))
                     .addChild(child)
             }
             .addSystem(.update) { (currentTime: Resource<CurrentTime>, commands: Commands, children: Query<Child>, parents: Query2<Entity, Parent>, totalEntities: Query<Entity>) in
@@ -191,9 +204,9 @@ final class GraphicPlugInTests: XCTestCase {
                 case 0:
                     flags[0] += 1
                     XCTAssertEqual(parents.components.data.count, 3)
-                    XCTAssertEqual(children.components.data.count, 0)
+                    XCTAssertEqual(children.components.data.count, 2)
                     XCTAssertEqual(totalEntities.components.data.count, 3)
-                case 1:
+                case 1: // FIXME: - 削除
                     flags[0] += 1
                     XCTAssertEqual(parents.components.data.count, 3)
                     XCTAssertEqual(children.components.data.count, 2)
@@ -216,7 +229,7 @@ final class GraphicPlugInTests: XCTestCase {
                     XCTAssertEqual(children.components.data.count, 0)
                     XCTAssertEqual(totalEntities.components.data.count, 0)
                     // このフレームの時だけ、world に存在しない 親を持つ子がいることになる.
-                case 4:
+                case 4: // FIXME: - 削除
                     flags[0] += 1
                     XCTAssertEqual(parents.components.data.count, 0)
                     XCTAssertEqual(children.components.data.count, 0)
@@ -229,10 +242,10 @@ final class GraphicPlugInTests: XCTestCase {
         world.setUpWorld()
         world.update(currentTime: -1)
         world.update(currentTime: 0)
-        world.update(currentTime: 1)
+        world.update(currentTime: 1) // FIXME: - 削除
         world.update(currentTime: 2)
         world.update(currentTime: 3)
-        world.update(currentTime: 4)
+        world.update(currentTime: 4)  // FIXME: - 削除
 
         XCTAssertEqual(flags, [5])
     }
@@ -244,12 +257,14 @@ final class GraphicPlugInTests: XCTestCase {
         let world = World()
             .addResource(SceneResource(scene))
             .addPlugIn(graphicPlugIn(world:))
-            .addSystem(.startUp) { (commands: Commands) in
+            .addSystem(.startUp) { (commands: Commands, nodes: Resource<Nodes>) in
+                let childNode = SKNode()
+                let parentNode = SKNode()
                 let child = commands.spawn()
-                    .setGraphic(SKNode())
+                    .setGraphic(nodes.resource.create(node: childNode))
                     .id()
                 commands.spawn()
-                    .setGraphic(SKNode())
+                    .setGraphic(nodes.resource.create(node: parentNode))
                     .addChild(child)
             }
             .addSystem(.update) { (currentTime: Resource<CurrentTime>, commands: Commands, children: Filtered<Query<Entity>, With<Child>>, parents: Query2<Entity, Parent>, totalEntities: Query<Entity>) in
@@ -258,9 +273,9 @@ final class GraphicPlugInTests: XCTestCase {
                 case 0:
                     flags[0] += 1
                     XCTAssertEqual(parents.components.data.count, 2)
-                    XCTAssertEqual(children.query.components.data.count, 0)
+                    XCTAssertEqual(children.query.components.data.count, 1)
                     XCTAssertEqual(totalEntities.components.data.count, 2)
-                case 1:
+                case 1: // FIXME: - 削除
                     flags[0] += 1
                     XCTAssertEqual(parents.components.data.count, 2)
                     XCTAssertEqual(children.query.components.data.count, 1)
@@ -287,7 +302,7 @@ final class GraphicPlugInTests: XCTestCase {
         world.setUpWorld()
         world.update(currentTime: -1)
         world.update(currentTime: 0)
-        world.update(currentTime: 1)
+        world.update(currentTime: 1) // FIXME: - 削除
         world.update(currentTime: 2)
         world.update(currentTime: 3)
 
