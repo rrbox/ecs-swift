c0f537cd6f91cb6615c1e9390a7b7fd5b3093d21
rrbox <87851278+rrbox@users.noreply.github.com>
2025-09-27 11:53:13 +0900

    Add FeatureFlags

---
+++ Sources/ECS/Commons/FeatureFlags.swift
@@ -0,0 +1,15 @@
+//
+//  FeatureFlags.swift
+//  ECS_Swift
+//
+//  Created by rrbox on 2025/09/27.
+//
+
+struct FeatureFlags: OptionSet {
+    let rawValue: UInt8
+    static let enabled: FeatureFlags = []
+
+    func isEnabled(_ flags: FeatureFlags) -> Bool {
+        Self.enabled.contains(flags)
+    }
+}
